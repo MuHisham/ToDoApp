abe72de5b5aca049f7d1076373ea98efff351837
MuHisham <172366361+MuHisham@users.noreply.github.com>
2024-09-06 23:10:33 +0400

    Feat: Added UI to the application

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+const addBtn = document.querySelector(".add-btn");
+const modalBox = document.querySelector(".modal");
+const closeBtn = document.querySelector(".close");
+
+addBtn.addEventListener("click", () => {
+    modalBox.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+    modalBox.style.display = "none";
+})
